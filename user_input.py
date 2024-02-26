d61df0df93c0a19b1a843b299a68082f81714083
zcun007 <160480881+zcun007@users.noreply.github.com>
2024-02-26 02:06:07 +0100

    user_input.py

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('Whats is your name ').strip().capitalize()
+age = int(input('How old are you '))
+location = input('What is your location ')
+print('Hello, ', name, 'you are ', age, 'years old and ', 'you live in ', location)
